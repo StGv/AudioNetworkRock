6875ec9cde2142f0d221360aae686a839ba52b8e
Stefan Georgiev <stefan.georgiev@sky.uk>
2018-11-25 22:48:21 +0000

    configuring return type to be Json

--- AudioNetworkRock/App_Start/WebApiConfig.cs
+++ AudioNetworkRock/App_Start/WebApiConfig.cs
@@ -16,6 +16,12 @@ namespace AudioNetworkRock
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
+                new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting =
+                Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
